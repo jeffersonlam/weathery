20ed0bbee628e7baedb679877380257a0d61511e
Jefferson Lam <jeffersonwlam@gmail.com>
2015-02-25 21:10:03 -0600

    updating master to be up-to-date with gh-pages

--- weathery.js
+++ weathery.js
@@ -1,9 +1,9 @@
 //Global Variables
 var xhr;
+var xhr2;
 var weatherAPIkey = "d594f9edbc2d1f2d90aedaa4cffa9f01";
 var giphyAPIkey = "TlK63EM2NT69sTlZCus";
-var cities = ['London', 'Bangkok', 'Paris', 'Singapore', 'Dubai', 'New York', 'Istanbul', 
-			  'Hong Kong', 'Seoul', 'Barcelona', 'Amsterdam', 'Rome', 'Tokyo', 'Austin Tx', 'San Francisco'];
+var cities = ['London', 'Bangkok', 'Paris', 'Singapore', 'Dubai', 'New York', 'Istanbul', 'Hong Kong', 'Seoul', 'Barcelona', 'Amsterdam', 'Rome', 'Tokyo', 'Austin Tx', 'San Francisco'];
 var keyPressTimeout;
 var prev;
 
@@ -13,37 +13,22 @@ window.onload = function(){
 	document.getElementById('city').value = city;
 	prev = city;
 	adjustWidth(document.getElementById('city'));
-	initialAPICalls(city);
+	weatherRequestAjax(city);
 }
 
-//initialAPICalls()
-//=================
-//Makes the initial API calls upon page load, getting and displaying weather and gif
-function initialAPICalls(city){
-	var response = weatherRequest(city);
-	displayWeather(response);
-	//response.weather[0].main contains a one word description of the weather
-	var response2 = gifRequest(response.weather[0].main);
-	displayGif(response2);
-}
-
-//adjustWidth()
-//=============
 //Adjusts width of input field dynamically
 function adjustWidth(obj) {
     obj.style.width = (obj.value.length) * 27 + "px";
 }
 
-//keyUp()
-//=======
-//Called on keyup event in input field. Adjusts input field width. Sends AJAX requests. Only sends request after finished typing for 400ms 
+//Called on keyup event in input field. Adjusts input field width. Sends AJAX requests. Only sends request after finished typing for 800ms 
 //Doesn't send AJAX requests on keypresses that aren't characters (eg alt, shift, capslock) except enter
 function keyUp(obj, event){
-	//if user presses enter and city value hasn't changed, update the gif
+	//if user presses 'enter' and city value hasn't changed, update the gif
 	if (prev == document.getElementById('city').value && event.keyCode == 13){
 		var hashtag = document.getElementById('hashtag').textContent.substring(1);
-		var response = gifRequest(hashtag);
-		displayGif(response);
+		// var response = gifRequest(hashtag);
+		// displayGif(response);
 	//if keyinput is detected but city value hasn't changed, do nothing
 	} else if (prev == document.getElementById('city').value){
 		return;
@@ -54,24 +39,10 @@ function keyUp(obj, event){
 		adjustWidth(obj);
 		keyPressTimeout = window.setTimeout(function(){
 			weatherRequestAjax(obj.value);
-		}, 400);
+		}, 800);
 	}
 }
 
-//weatherRequest()
-//================
-//Given a city, searches for a city in the weather API. Synchronous. Used in initial page load
-function weatherRequest(city){
-	var request = new XMLHttpRequest();
-	var url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&APPID="+weatherAPIkey;
-	request.open("GET", url, false);
-	request.send();
-	var response = JSON.parse(request.responseText);
-	return response;
-}
-
-//weatherRequestAjax()
-//====================
 //Given a city, searches for a city in the weather API. Asynchronous. Called in keyup. Also calls gif requests
 function weatherRequestAjax(city){
 	xhr = new XMLHttpRequest();
@@ -87,12 +58,12 @@ function weatherRequestAjax(city){
 	    		//check if city exists
 	    		if (response.cod == 404){
 	    			console.error(response.message);
+					displayError();
 	    			return;
 	    		} 
 	      		displayWeather(response);
 	      		//response.weather[0].main contains a one word description of the weather
-	      		var response2 = gifRequest(response.weather[0].main);
-	      		displayGif(response2);
+	      		gifRequestAjax(response.weather[0].main);
 	    	} else {
 	      		console.error(xhr.statusText);
 	    	}
@@ -103,8 +74,6 @@ function weatherRequestAjax(city){
 	}
 }
 
-//displayWeather()
-//============
 //Given a weather API response, displays elements on page
 function displayWeather(response){
 	var hashtag = response.weather[0].main;
@@ -117,19 +86,33 @@ function displayWeather(response){
 	document.getElementById("temperature").innerHTML = temperature + "&#176;";
 }
 
-//gifRequest()
-//============
-//Given a hashtag, searches for a gif on Giphy (limit 100). Synchronous
-function gifRequest(hashtag){
-	var request = new XMLHttpRequest();
-	request.open("GET", "http://api.giphy.com/v1/gifs/search?q="+hashtag+"&limit=100&api_key="+giphyAPIkey, false);
-	request.send();
-	var response = JSON.parse(request.responseText);
-	return response;
+function displayError(){
+	document.getElementById("description").innerHTML = "City not found";
+	document.getElementById("temperature").innerHTML = "";
+	document.getElementById("hashtag").innerHTML = "&nbsp;";
 }
 
-//displayGif()
-//============
+function gifRequestAjax(hashtag){
+	xhr2 = new XMLHttpRequest();
+	xhr2.open("GET", "http://api.giphy.com/v1/gifs/search?q="+hashtag+"&limit=100&api_key="+giphyAPIkey, true);
+	xhr2.onreadystatechange = callback;
+	xhr2.send();
+
+	function callback() {
+	  	if (xhr2.readyState === 4) {
+	    	if (xhr2.status === 200) {
+	    		var gifResponse = JSON.parse(xhr2.responseText);;
+	      		displayGif(gifResponse);
+	    	} else {
+	      		console.error(xhr2.statusText);
+	    	}
+	  	}
+		xhr2.onerror = function (e) {
+		  console.error(xhr.statusText);
+		};
+	}
+}
+
 //Given a Giphy API response, chooses a random gif, and displays it on page
 function displayGif(response){
 	var gif = randomElement(response.data);
@@ -141,8 +124,6 @@ function displayGif(response){
 	// document.body.style.backgroundImage = "url('"+gifURL+"')";
 }
 
-//randomElement()
-//===============
 //Given an array, returns a random element from the array
 function randomElement(array){
 	var num = Math.floor(Math.random() * array.length);
